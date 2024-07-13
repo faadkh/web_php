daa4504c7acfd5f5593ac377793f0895b03417f0
faadkh <faadkh05@gmail.com>
2024-07-13 20:38:17 +0500

    now start database

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="main">
-        <div class="nav">
-            <h1>PHP PROJECT FOR PERECTICE</h1>
+        <div class="nav1">
+            <h1 class="">PHP PROJECT FOR PERECTICE</h1>
         </div>
         <div class="sidebar">
             <h2>Actions</h2>
@@ -20,7 +22,40 @@
             </ul>
         </div>
         <div class="body_main">
-
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">S_No</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">DOB</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>Fahad</td>
+                    <td>faadkh05@gmail.com</td>
+                    <td>03168927749</td>
+                    <td>05/10/1999</td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>Faraz</td>
+                    <td>farazkh01@gmail.com</td>
+                    <td>03168928849</td>
+                    <td>05/10/1997</td>
+                </tr>
+                <tr>
+                    <td>03</td>
+                    <td>Farhan</td>
+                    <td>farhan@gmail.com</td>
+                    <td>03168921239</td>
+                    <td>05/10/2000</td>
+                </tr>
+            </tbody>
+        </table>
         </div>
     </div>   
 
